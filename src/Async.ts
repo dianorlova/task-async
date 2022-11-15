716338aede88a7db71e6c233bfc434b00f1caaa5
dianorlova <dianaorlova0101@gmail.com>
2022-11-15 16:32:47 +0300

    task-async-commit

--- src/Async.ts
+++ src/Async.ts
@@ -3,6 +3,11 @@
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            return resolve(ms);
+        }, ms);
+    });
 }
 
 /*
@@ -11,19 +16,10 @@ export function mock(ms: number): Promise<number> {
 export function getData(): Promise<number[]> {
     const result: number[] = [];
 
-    return mock(100)
-        .then((data1) => {
-            result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
-            result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
-            result.push(data3);
-            return result;
-        });
+    return Promise.all([mock(100), mock(200), mock(300)]).then((results) => {
+        results.forEach((number) => result.push(number));
+        return result;
+    });
 }
 
 /*
@@ -32,8 +28,10 @@ export function getData(): Promise<number[]> {
  */
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
+        await Promise.reject(new Error('my error'));
     } catch (err) {
-        return err.message;
+        if (err instanceof Error) {
+            return err.message;
+        }
     }
 }
